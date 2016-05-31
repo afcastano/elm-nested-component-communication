8e90c308757f010245787812f720365f53dadfd8
Roman Frołow <rofrol@gmail.com>
2016-05-31 23:57:29 +0200

    use elm format and add .editorconfig

--- src/Pair.elm
+++ src/Pair.elm
@@ -1,50 +1,60 @@
 module Pair exposing (Model, init, view, Msg(UpdateRed), update, getRedNum)
 
 import Counter
-
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as App
 
+
 (=>) : a -> b -> ( a, b )
-(=>) = (,)
+(=>) =
+    (,)
+
 
 type alias Model =
-  { greenCounter : Counter.Model
-  , redCounter : Counter.Model
-  , totalClickCount : Int
-  }
+    { greenCounter : Counter.Model
+    , redCounter : Counter.Model
+    , totalClickCount : Int
+    }
+
 
 init : Model
 init =
-  Model (Counter.init 0) (Counter.init 0) 0
+    Model (Counter.init 0) (Counter.init 0) 0
+
 
 view : Model -> Html Msg
 view model =
-  div [ style ["background-color" => "lightgray", "margin-bottom" => "1rem"] ]
-    [ div [][text <| "Total click count " ++ (toString model.totalClickCount)]
-    , App.map PairGreen (Counter.view "green" model.greenCounter)
-    , App.map PairRed (Counter.view "red" model.redCounter)
-    ]
+    div [ style [ "background-color" => "lightgray", "margin-bottom" => "1rem" ] ]
+        [ div [] [ text <| "Total click count " ++ (toString model.totalClickCount) ]
+        , App.map PairGreen (Counter.view "green" model.greenCounter)
+        , App.map PairRed (Counter.view "red" model.redCounter)
+        ]
+
 
 type Msg
-  = PairRed Counter.Msg
-  | PairGreen Counter.Msg
-  | UpdateRed Int
+    = PairRed Counter.Msg
+    | PairGreen Counter.Msg
+    | UpdateRed Int
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    PairGreen sub ->
-      { model | greenCounter = Counter.update sub model.greenCounter, totalClickCount = model.totalClickCount + 1 }
+    case msg of
+        PairGreen sub ->
+            { model | greenCounter = Counter.update sub model.greenCounter, totalClickCount = model.totalClickCount + 1 }
 
-    PairRed sub ->
-      { model | redCounter = Counter.update sub model.redCounter, totalClickCount = model.totalClickCount + 1 }
+        PairRed sub ->
+            { model | redCounter = Counter.update sub model.redCounter, totalClickCount = model.totalClickCount + 1 }
 
-    UpdateRed value ->
-      { model | redCounter = Counter.update (Counter.SetNum value) model.redCounter }
+        UpdateRed value ->
+            { model | redCounter = Counter.update (Counter.SetNum value) model.redCounter }
+
+
 
 ------- INTEFACE HELPERS
+
+
 getRedNum : Model -> Int
 getRedNum model =
-  Counter.getNum model.redCounter
+    Counter.getNum model.redCounter
